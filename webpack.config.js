9c72792ff0a338f2f9294510c011ba1bfd60a802
Michele Bertoli <michele@berto.li>
2015-07-02 18:17:55 +0100

    hot

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
-var path = require('path');
+var webpack = require("webpack");
 
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/dev-server',
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: __dirname + '/dist',
     filename: 'bundle.js',
     publicPath: '/assets/'
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };

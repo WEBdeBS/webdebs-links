62261680fe94eee4c802e1c01622c92d0e60a0a5
Michele Bertoli <michele@berto.li>
2015-07-03 18:56:49 +0100

    express

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var webpack = require("webpack");
-var config = require('./webpack.config');
-var WebpackDevServer = require("webpack-dev-server");
+var webpackMiddleware = require("webpack-dev-middleware");
+var hmr = require("webpack-dev-hmr");
+var express = require('express');
 
+var app = express();
+var config = require('./webpack.config');
 var compiler = webpack(config);
-var server = new WebpackDevServer(compiler, {
+
+app.use(webpackMiddleware(compiler, {
   publicPath: config.output.publicPath
+}));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
-server.listen(3000, "localhost", function() {
-  console.log('Listening at localhost:3000');
+
+var server = app.listen(3000, 'localhost', function() {
+  hmr.listen(server, compiler);
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Listening at http://%s:%s', host, port);
 });

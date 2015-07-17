81f89e33fafb3bd909732700e3e948f11dbe4d82
Michele Bertoli <michele@berto.li>
2015-07-17 21:55:48 +0100

    cors

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,11 @@
 import request from 'superagent';
 import {promise} from 'when';
 
-const proxy = 'http://crossorigin.me/';
 const url = 'http://safe-woodland-6446.herokuapp.com/links?page=';
 
 export function getLinks(page) {
   return promise((resolve, reject) => {
-    request.get(`${proxy}${url}${page}`).end((err, res) => {
+    request.get(`${url}${page}`).end((err, res) => {
       if (err) {
         reject(err);
       } else {

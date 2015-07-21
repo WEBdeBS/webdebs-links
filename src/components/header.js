1a6a00fad60c7193d169a04794bffec1c9195008
Michele Bertoli <michele@berto.li>
2015-07-21 18:07:32 +0100

    header

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
 
   render() {
     return (
-      <div className={styles.header}></div>
+      <a className={styles.header} href="/"></a>
     );
   }
 

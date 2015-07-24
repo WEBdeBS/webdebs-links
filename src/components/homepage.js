023305f8f167b59ee870c156bbfb6936a5cc94d6
Michele Bertoli <michele@berto.li>
2015-07-24 22:01:30 +0100

    marquee

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -17,6 +17,28 @@ export default class Homepage extends Component {
     return getLinks(state.params.page || defaultPage);
   }
 
+  renderMarquee1() {
+    const marquee = `
+      <marquee class=${styles.marquee} direction="down" behavior="alternate">
+        <marquee behavior="alternate">#OLD</marquee>
+      </marquee>
+    `;
+    return (
+      <div dangerouslySetInnerHTML={{__html:marquee}}></div>
+    );
+  }
+
+  renderMarquee2() {
+    const marquee =  `
+      <marquee class=${styles.marquee} scrolldelay="200" direction="up" behavior="alternate">
+        <marquee scrolldelay="200" behavior="alternate">Spiedo</marquee>
+      </marquee>
+    `;
+    return (
+      <div dangerouslySetInnerHTML={{__html:marquee}}></div>
+    );
+  }
+
   render() {
     const {data, pagination} = this.props.data.homepage;
     const storage = new Storage(dough(this.props.req));
@@ -27,6 +49,8 @@ export default class Homepage extends Component {
         <Links items={data} storage={storage} />
         <Pagination current={page} total={pagination.total} />
         <div className={styles.frame}></div>
+        {this.renderMarquee1()}
+        {this.renderMarquee2()}
       </div>
     );
   }

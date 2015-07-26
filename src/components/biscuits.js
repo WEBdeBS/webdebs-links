657d3a9e135f8b0b679c25e8075d928b852bf2a8
Michele Bertoli <michele@berto.li>
2015-07-26 17:16:18 +0100

    ¯\_(ツ)_/¯

--- src/components/biscuits.js
+++ src/components/biscuits.js
@@ -6,20 +6,43 @@ const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 export default class Biscuits extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      mounted: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      mounted: true
+    });
+  }
+
   render() {
+    let cookie0 = null;
+    let cookie1 = null;
+    let credits = null;
+    let cookie2 = null;
+    if (this.state.mounted) {
+      cookie0 = <span key='cookie' className='cookie cookie--0'></span>;
+      cookie1 = <span key='cookie' className='cookie cookie--1'></span>;
+      credits = <span key='credits' className='credits'></span>;
+      cookie2 = <span key='cookie' className='cookie cookie--2'></span>;
+    }
     return (
       <div>
-        <ReactCSSTransitionGroup transitionName="cookie--0" transitionAppear={true}>
-          <span key='cookie' className='cookie cookie--0'></span>
+        <ReactCSSTransitionGroup transitionName="cookie--0">
+          {cookie0}
         </ReactCSSTransitionGroup>
-        <ReactCSSTransitionGroup transitionName="cookie--1" transitionAppear={true}>
-          <span key='cookie' className='cookie cookie--1'></span>
+        <ReactCSSTransitionGroup transitionName="cookie--1">
+          {cookie1}
         </ReactCSSTransitionGroup>
-        <ReactCSSTransitionGroup transitionName="credits" transitionAppear={true}>
-          <span key='credits' className='credits'></span>
+        <ReactCSSTransitionGroup transitionName="credits">
+          {credits}
         </ReactCSSTransitionGroup>
-        <ReactCSSTransitionGroup transitionName="cookie--2" transitionAppear={true}>
-          <span key='cookie' className='cookie cookie--2'></span>
+        <ReactCSSTransitionGroup transitionName="cookie--2">
+          {cookie2}
         </ReactCSSTransitionGroup>
       </div>
     );
